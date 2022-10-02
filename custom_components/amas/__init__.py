a065fe5abb0cb3d30923a2f53fd6bc4e4be12263
meghadeep-com <contact@meghadeep.com>
2022-10-01 19:47:16 -0600

    async fixes

--- custom_components/amas/__init__.py
+++ custom_components/amas/__init__.py
@@ -83,7 +83,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     api = AMASHub(host, hass, async_create_clientsession(hass))
     if await api.authenticate(api_key):
         device_info = await api.get_data()
-        await hass.config_entries.async_update_entry(entry, unique_id='AMAS-'+str(device_info['dev_id']))
+        hass.config_entries.async_update_entry(entry, unique_id=('AMAS-'+str(device_info['dev_id'])))
     
     # TODO 3. Store an API object for your platforms to access
     # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
