29ce24bc2919ea875b086fb25ae0187951e4cb29
meghadeep-com <contact@meghadeep.com>
2022-10-02 13:33:44 -0600

    Fixed control device

--- custom_components/amas/const.py
+++ custom_components/amas/const.py
@@ -139,7 +139,7 @@ class AMASHub:
         try:
             # r = requests.post(url, headers=headers, body=body)
             async with async_timeout.timeout(10):
-                response = await self.session.post(url, headers=headers, body=body)
+                response = await self.session.post(url, headers=headers, data=body)
                 _LOGGER.debug("Response content: %s", str(response.content))
             if response.status == 200:
                 device_info = await response.json()
