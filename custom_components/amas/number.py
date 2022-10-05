e581e1a9ab1691034ff1cf4950e01a9e72fab304
meghadeep-com <contact@meghadeep.com>
2022-10-05 00:20:59 -0600

    Fixed UTC time handling

--- custom_components/amas/number.py
+++ custom_components/amas/number.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import logging
 from typing import Any
+import datetime
 
 
 from homeassistant.config_entries import ConfigEntry
@@ -10,6 +11,7 @@ from homeassistant.const import CONF_NAME
 
 from homeassistant.components.number import NumberEntity, NumberMode
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConditionError
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
@@ -70,14 +72,70 @@ class AMASNumber(AMASTechEntity, NumberEntity):
     def native_value(self) -> Any:
         """Return the state of the device."""
         act_key = str(self.entity_description.key).split('_')[1]
-        return str(self.api.device_info['light'][act_key])
+        utc_aware = datetime.datetime.utcnow().astimezone(datetime.datetime.utcnow().astimezone().tzinfo)
+        local_aware = datetime.datetime.now().astimezone()
+        time_delta = local_aware - utc_aware
+        time_delta = time_delta.days*24*3600 + time_delta.seconds
+        time_delta_min = int(time_delta/60)
+        delta_min = time_delta_min%60
+        delta_hour = time_delta_min//60
+        native = str(self.api.device_info['light'][act_key])
+        _LOGGER.debug("Got UTC value light control " + act_key + ': ' + native)
+        native_hour = native[0]+native[1]
+        native_min = native[2]+native[3]
+        converted_min = int(int(native_min) + delta_min)
+        if converted_min >= 60:
+            converted_hour = int(1 + delta_hour + int(native_hour))
+            converted_min = int(converted_min%60)
+        elif converted_min < 60 and converted_min >= 0:
+            converted_hour = int(delta_hour + int(native_hour))
+        if converted_hour < 0:
+            converted_hour = int(converted_hour + 24)
+        elif converted_hour >= 24:
+            converted_hour = int(converted_hour - 24)
+        converted_min = str(converted_min) if len(str(converted_min)) == 2 else '0' + str(converted_min)
+        converted_hour = str(converted_hour) if len(str(converted_hour)) == 2 else '0' + str(converted_hour)
+        return converted_hour + converted_min
 
     async def async_set_native_value(self, value: int) -> None:
         """Update the current value."""
         try:
             act_key = str(self.entity_description.key).split('_')[1]
-            _LOGGER.debug("Sending light control " + act_key)
-            await self.api.control_device({'light': {act_key: value}})
+            _LOGGER.debug("Got local value light control " + act_key + ': ' + value)
+            value = str(value)
+            if len(value) == 1:
+                value = '000' + value
+            elif len(value) == 2:
+                value = '00' + value
+            elif len(value) == 3:
+                value = '0' + value
+            if int(value[-2]+value[-1]) >= 60:
+                raise ConditionError
+            utc_aware = datetime.datetime.utcnow().astimezone(datetime.datetime.utcnow().astimezone().tzinfo)
+            local_aware = datetime.datetime.now().astimezone()
+            time_delta = utc_aware - local_aware
+            time_delta = time_delta.days*24*3600 + time_delta.seconds
+            time_delta_min = int(time_delta/60)
+            delta_min = time_delta_min%60
+            delta_hour = time_delta_min//60
+            native = value
+            native_hour = native[0]+native[1]
+            native_min = native[2]+native[3]
+            converted_min = int(int(native_min) + delta_min)
+            if converted_min >= 60:
+                converted_hour = int(1 + delta_hour + int(native_hour))
+                converted_min = int(converted_min%60)
+            elif converted_min < 60 and converted_min >= 0:
+                converted_hour = int(delta_hour + int(native_hour))
+            if converted_hour < 0:
+                converted_hour = int(converted_hour + 24)
+            elif converted_hour >= 24:
+                converted_hour = int(converted_hour - 24)
+            converted_min = str(converted_min) if len(str(converted_min)) == 2 else '0' + str(converted_min)
+            converted_hour = str(converted_hour) if len(str(converted_hour)) == 2 else '0' + str(converted_hour)
+            military = converted_hour + converted_min
+            _LOGGER.debug("Sending light control " + act_key + ': ' + military)
+            await self.api.control_device({'light': {act_key: military}})
             await self.async_update()
         except Exception as err:
             _LOGGER.error("Unable to turn on light control " + act_key + " : %s", err)
